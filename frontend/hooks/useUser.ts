65b70dbb208c11438f2014e8e6f70aa6afb0d239
amir-haghighi <hagh73h@gmail.com>
2026-06-19 13:50:50 -0700

    minor stale time

--- frontend/hooks/useUser.ts
+++ frontend/hooks/useUser.ts
@@ -24,7 +24,7 @@ export function useUser() {
         queryKey: ["me"],
         queryFn: fetchMe,
         retry: false,
-        staleTime: 1000 * 60 * 5, //5min
+        staleTime: 1000 * 30, // 30s
     });
 
 
